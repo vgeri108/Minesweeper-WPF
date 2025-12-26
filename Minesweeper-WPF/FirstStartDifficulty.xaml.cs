16f085758dbecb2c251b43a24fcd06acefcac8c3
vgeri108 <vagany.gergely@gmail.com>
2025-12-26 10:02:42 +0100

    Első indítás menüje kész
    Magyarra le van fordítva és a gombjai is működnek.

--- Minesweeper-WPF/FirstStartDifficulty.xaml.cs
+++ Minesweeper-WPF/FirstStartDifficulty.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,11 +19,42 @@ namespace Minesweeper_WPF
     /// <summary>
     /// Interaction logic for FirstStartDifficulty.xaml
     /// </summary>
+    
     public partial class FirstStartDifficulty : Window
     {
         public FirstStartDifficulty()
         {
             InitializeComponent();
+        }
+
+        private void NewBeginner(object sender, RoutedEventArgs e)
+        {
+            Data.Difficulty = "Easy";
+            Data.meretM = 9;
+            Data.meretSZ = 9;
+            Data.aknakszama = 10;
+            BoardManager.Init();
+            Close();
+        }
+
+        private void NewIntermediate(object sender, RoutedEventArgs e)
+        {
+            Data.Difficulty = "Intermediate";
+            Data.meretM = 16;
+            Data.meretSZ = 16;
+            Data.aknakszama = 40;
+            BoardManager.Init();
+            Close();
+        }
+
+        private void NewAdvanced(object sender, RoutedEventArgs e)
+        {
+            Data.Difficulty = "Advanced";
+            Data.meretM = 16;
+            Data.meretSZ = 30;
+            Data.aknakszama = 99;
+            BoardManager.Init();
+            Close();
         }
     }
 }
