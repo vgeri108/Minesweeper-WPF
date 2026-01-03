3a5dee91d64a299009390985708feee48ab3c316
vgeri108 <vagany.gergely@gmail.com>
2026-01-03 16:33:15 +0100

    Mezők lekorlátozása I.
    A beállítások szövegdobozaiba már nem lehet írni betűket, vesszőket, pontokat, szóközöket, stb...
    Amikor elvesztik a fókuszt, ha a benne lévő érték meghaladja a minimumot, a maximumot akkor automatikusan beírja a megfelelőt. Ugyanez a helyzet ha nincs beírva semmi se.

--- Minesweeper-WPF/Settings.xaml.cs
+++ Minesweeper-WPF/Settings.xaml.cs
@@ -112,12 +112,62 @@ namespace Minesweeper_WPF
                     Data.aknakszama = int.Parse(tbMines.Text);
                 } catch (Exception error)
                 {
-                    MessageBox.Show(error.Message, "Hiba",MessageBoxButton.OK, MessageBoxImage.Error); //le kell tírtani hogy pontot, vesszűt betűt,stb... ne engedjen beírni
+                    MessageBox.Show(error.Message, "Hiba",MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
             JsonManager.Settings.Save();
             BoardManager.Init();
             Close();
+        }
+        private void tbHeight_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int value;
+            bool Valid = int.TryParse(tbHeight.Text, out value);
+            if ((!Valid || tbHeight.Text.Contains(' ')) && tbHeight.Text.Length > 0)
+            {
+                tbHeight.Text = tbHeight.Text.Remove(tbHeight.Text.Length - 1);
+                tbHeight.CaretIndex = tbHeight.Text.Length;
+            }
+        }
+
+        private void tbWidth_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int value;
+            bool Valid = int.TryParse(tbWidth.Text, out value);
+            if ((!Valid || tbWidth.Text.Contains(' ')) && tbWidth.Text.Length > 0)
+            {
+                tbWidth.Text = tbWidth.Text.Remove(tbWidth.Text.Length - 1);
+                tbWidth.CaretIndex = tbWidth.Text.Length;
+            }
+        }
+
+        private void tbMines_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int value;
+            bool Valid = int.TryParse(tbMines.Text, out value);
+            if ((!Valid || tbMines.Text.Contains(' ')) && tbMines.Text.Length > 0)
+            {
+                tbMines.Text = tbMines.Text.Remove(tbHeight.Text.Length - 1);
+                tbMines.CaretIndex = tbHeight.Text.Length;
+            }
+        }
+        private void tbHeight_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbHeight.Text.Length == 0) tbHeight.Text = "9";
+            if (Convert.ToInt32(tbHeight.Text) < 9) tbHeight.Text = "9";
+            if (Convert.ToInt32(tbHeight.Text) > 14) tbHeight.Text = "24";
+        }
+        private void tbWidth_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbWidth.Text.Length == 0) tbWidth.Text = "9";
+            if (Convert.ToInt32(tbWidth.Text) < 9) tbWidth.Text = "9";
+            if (Convert.ToInt32(tbWidth.Text) > 14) tbWidth.Text = "30";
+        }
+        private void tbMines_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbMines.Text.Length == 0) tbMines.Text = "10";
+            if (Convert.ToInt32(tbMines.Text) < 9) tbMines.Text = "10";
+            if (Convert.ToInt32(tbMines.Text) > 14) tbMines.Text = "668";
         }
     }
 }
--- Minesweeper-WPF/obj/Debug/net8.0-windows/Minesweeper-WPF.AssemblyInfo.cs
+++ Minesweeper-WPF/obj/Debug/net8.0-windows/Minesweeper-WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minesweeper-WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6477dc865b786b8f0558dd2e8099ad04060b9c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec8150c99f78fd446e156d7bdf55257c266fb38b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minesweeper-WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minesweeper-WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
