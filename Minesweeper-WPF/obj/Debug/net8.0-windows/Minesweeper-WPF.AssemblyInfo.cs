7fdc119f1bf1b9ceb00caa616f9a4e83d90bb569
vgeri108 <vagany.gergely@gmail.com>
2026-01-05 18:54:38 +0100

    AppIcon hozzáadva
    Ennek még nincsen nagy jelentősége, csak annyi, hogy fájlkezelőkben és az asztalon ez az ikon fog látszódni. Ennek később a telepítőnél lesz szerepe, hogy az asztalon lévő parancsikonnál ez az ikon legyen. Továbbá a jövőben szeretném azt, hogy a tálcán külön ikon legyen, ne az ami az ablak ikonja. A Windows7-es aknakeresőben is így volt.

--- Minesweeper-WPF/obj/Debug/net8.0-windows/Minesweeper-WPF.AssemblyInfo.cs
+++ Minesweeper-WPF/obj/Debug/net8.0-windows/Minesweeper-WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minesweeper-WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf2b8e5ddeef945d98f37724cf24eee46f89b226")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2883b9d6095c83c32929e23fe4abb2d964ed9827")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minesweeper-WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minesweeper-WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
