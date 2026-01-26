fd9b0fba152cb6adceb7d861d82505df9fdef5a4
vgeri108 <vagany.gergely@gmail.com>
2026-01-26 17:41:30 +0100

    Statisztikák IX: Idő csak nyeréskor mentődik
    Most már a játék ideje csak akkor mentődik el ha a játék meg lett nyerve.

--- Minesweeper-WPF/BoardManager.cs
+++ Minesweeper-WPF/BoardManager.cs
@@ -454,6 +454,8 @@ namespace Minesweeper_WPF
             }
             else
             {
+                Statistics.Times[Statistics.currentMode].Add(Time.ElapsedSeconds);
+                Statistics.Dates[Statistics.currentMode].Add(DateTime.Now.ToString("yyyy/MM/dd"));
                 dialog = new GameWin();
             }
 
@@ -484,8 +486,7 @@ namespace Minesweeper_WPF
 
             if (Statistics.WinStreak[Statistics.currentMode] > Statistics.LongestWinStreak[Statistics.currentMode]) Statistics.LongestWinStreak = Statistics.WinStreak;
             if (Statistics.LoseStreak[Statistics.currentMode] > Statistics.LongestLoseStreak[Statistics.currentMode]) Statistics.LongestLoseStreak = Statistics.LoseStreak;
-            Statistics.Times[Statistics.currentMode].Add(Time.ElapsedSeconds);
-            Statistics.Dates[Statistics.currentMode].Add(DateTime.Now.ToString("yyyy/MM/dd"));
+            
             JsonManager.Stats.Save();
             dialog.Owner = mw;
             dialog.ShowInTaskbar = true;
