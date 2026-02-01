58ecf49743b620eed733c7a6ae3103523a0e76d8
vgeri108 <vagany.gergely@gmail.com>
2026-02-01 15:51:31 +0100

    Témák V.: Téma adatainak regisztrálása
    Téma neve, képe, valamint bemutató képei szerepelnek az ImageNames szótárban.
    Commentekkel választottam el az elkülönülő témákat.

--- Minesweeper-WPF/Appearance.cs
+++ Minesweeper-WPF/Appearance.cs
@@ -24,6 +24,12 @@ namespace Minesweeper_WPF
         {
             public static Dictionary<string, string> ImageNames = new Dictionary<string, string>()
             {
+                { "_comment1", "----- Téma beállításai -----" },
+                { "ThemeName", "Alapértelmezett" },
+                { "ThemeImage", "pack.png" },
+
+                { "_comment2", "----- Pálya kinézete -----" },
+                { "Hatter", "Images/GUI/hatter.png" },
                 { "error", "Images/!noTexture.png" },
                 { "fedes", "Images/Covered.png" },
                 { "zaszlozott", "Images/Flagged.png" },
@@ -41,14 +47,22 @@ namespace Minesweeper_WPF
                 { "7", "Images/7.png" },
                 { "8", "Images/8.png" },
 
+                { "_comment3", "----- Pálya alatti sáv -----" },
                 { "Clock", "Images/GUI/clock.png" },
                 { "Flower", "Images/GUI/flower.png" },
-                { "Hatter", "Images/GUI/hatter.png" },
+
+                { "_comment4", "----- Névjegy menü feliratai -----" },
                 { "win?", "Images/GUI/windows.png" },
                 { "win7", "Images/GUI/windows7.png" },
                 { "win8", "Images/GUI/windows8.png" },
                 { "win10", "Images/GUI/windows10.png" },
                 { "win11", "Images/GUI/windows11.png" },
+
+                { "_comment5", "----- Téma módosítás ikonjai -----" },
+                { "Board", "Images/GUI/SelectedImage.png" },
+                { "Mines", "Images/Mine.png" },
+                { "Numbers", "Images/1.png" },
+                { "Background", "Images/GUI/hatter.png" },
             };
 
             // Try resolving the path to an existing file in several likely locations under the app output folder.
@@ -89,6 +103,8 @@ namespace Minesweeper_WPF
                 return new Uri(relUri, UriKind.Relative);
             }
 
+            public static Uri ThemeName => ResolveUri(ImageNames["ThemeName"]);
+            public static Uri ThemeImage => ResolveUri(ImageNames["ThemeImage"]);
             public static Uri error => ResolveUri(ImageNames["error"]);
             public static Uri fedes => ResolveUri(ImageNames["fedes"]);
             public static Uri zaszlozott => ResolveUri(ImageNames["zaszlozott"]);
@@ -114,6 +130,11 @@ namespace Minesweeper_WPF
             public static Uri windows8 => ResolveUri(ImageNames["win8"]);
             public static Uri windows10 => ResolveUri(ImageNames["win10"]);
             public static Uri windows11 => ResolveUri(ImageNames["win11"]);
+
+            public static Uri Board => ResolveUri(ImageNames["Board"]);
+            public static Uri Mines => ResolveUri(ImageNames["Mines"]);
+            public static Uri Numbers => ResolveUri(ImageNames["Numbers"]);
+            public static Uri Background => ResolveUri(ImageNames["Background"]);
         }
     }
 }
