227b1c4f53a0bcdb25df128d1e3476a9f81a78fc
vgeri108 <vagany.gergely@gmail.com>
2026-02-20 18:21:55 +0100

    Alpha 1.4.7 build
     ---===### Aknakereső ###===---
    
    Verzió: Alpha 1.4.7
    
    === Frissítések ===
    Új kinézet:
     => Klasszikus
    
    Hibajavítások:
     => Issue #20 javítva: Jááték közben is lehet hangot változtatni a témával

--- Minesweeper-WPF/Data.cs
+++ Minesweeper-WPF/Data.cs
@@ -61,9 +61,9 @@ namespace Minesweeper_WPF
     public class Version
     {
         public static bool FirstStart = true;
-        public static string Game = "Alpha 1.4.6";
+        public static string Game = "Alpha 1.4.7";
         public static string Json = Game;
-        public static string GithubTag = "vA1.4.6";
+        public static string GithubTag = "vA1.4.7";
     }
     public class Statistics
     {
