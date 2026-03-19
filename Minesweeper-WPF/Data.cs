e130c4b6cf48b6ebec0d900b82e5d090c0d7d7fc
vgeri108 <vagany.gergely@gmail.com>
2026-03-19 18:37:35 +0100

    Alpha 1.5.2 build
     ---===### Aknakereső ###===---
    
    Verzió: Alpha 1.5.2
    
    === Frissítések ===
    Minden klikkeléshez és zászló lehelyezéshez lehet hangot adni.
    A hangnak a témában kell szerepelnie, és jelenleg egyik téma sem tartalmaz!

--- Minesweeper-WPF/Data.cs
+++ Minesweeper-WPF/Data.cs
@@ -61,9 +61,9 @@ namespace Minesweeper_WPF
     public class Version
     {
         public static bool FirstStart = true;
-        public static string Game = "Alpha 1.5.1";
+        public static string Game = "Alpha 1.5.2";
         public static string Json = Game;
-        public static string GithubTag = "vA1.5.1";
+        public static string GithubTag = "vA1.5.2";
     }
     public class Statistics
     {
