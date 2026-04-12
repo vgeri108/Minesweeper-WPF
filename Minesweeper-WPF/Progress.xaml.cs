dc4bb0e9e84b52c7997a169324b261f1cdff6d0e
vgeri108 <vagany.gergely@gmail.com>
2026-04-12 10:11:24 +0200

    Folyamatjelző ablak
    Az ablak példányosításakor meg lehet adni, hogy mi legyen a címe és az üzenete.

---
+++ Minesweeper-WPF/Progress.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Minesweeper_WPF
+{
+    /// <summary>
+    /// Interaction logic for Progress.xaml
+    /// </summary>
+    public partial class Progress : Window
+    {
+        public Progress(string title = "Folyamat", string message = "Kérjük várjon...")
+        {
+            InitializeComponent();
+            Title = title;
+            MessageBlock.Text = message;
+            DataContext = this;
+            this.Title = title;
+        }
+    }
+}
