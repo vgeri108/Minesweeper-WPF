f5abd2f6c480f4ceade4e761a1138e31bc5714f4
vgeri108 <vagany.gergely@gmail.com>
2026-07-21 11:59:42 +0200

    Súgó ablak V.
    Szövegek átírása, linkek kattinthatóvá tétele, apró vizuális javítások.

--- Minesweeper-WPF/Help.xaml.cs
+++ Minesweeper-WPF/Help.xaml.cs
@@ -22,5 +22,41 @@ namespace Minesweeper_WPF
             InitializeComponent();
             BoardShowcase.Source = new BitmapImage(Appearance.Images.HelpWindowBoard);
         }
+
+        private async void JatekBeallitasModositas_Click(object sender, RoutedEventArgs e)
+        {
+            Inditas.IsExpanded = false;
+            Mentes.IsExpanded = false;
+            JatekBeallitasok.IsExpanded = true;
+            Megjelenes.IsExpanded = false;
+
+            JatekBeallitasok.BringIntoView();
+
+            await HighlightExpander(JatekBeallitasok);
+        }
+
+        private async Task HighlightExpander(Expander expander)
+        {
+            Brush eredetiSzin = expander.Foreground;
+            FontWeight eredetiBetu = expander.FontWeight;
+
+            for (int i = 0; i < 3; i++)
+            {
+                expander.Foreground = Brushes.OrangeRed;
+                expander.FontWeight = FontWeights.ExtraBold;
+
+                await Task.Delay(200);
+
+                expander.Foreground = eredetiSzin;
+                expander.FontWeight = eredetiBetu;
+
+                await Task.Delay(200);
+            }
+        }
+
+        private void EgyediTema_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ez a súgótéma még nem érhető el.", "Súgó", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
     }
 }
